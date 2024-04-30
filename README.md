187cb87247fe2b14901df8ae8565a57ada2300b2
Giovana Almeida <162519099+giovanadealmeida@users.noreply.github.com>
2024-04-30 20:04:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio-2
+ code-explorer
---
+++ script.js
@@ -0,0 +1,10 @@
+const fulimgbox = document.getElementById("fulimgbox"),
+fulimg = document.getElementById("fulimg");
+
+function openfulimg(reference){
+    fulimgbox.style.display = "flex";
+    fulimg.src = reference
+}
+function closeimg(){
+    fulimgbox.style.display = "none";
+}
